972aee1df88004660ac6d371724c738e0299d67b
Sam Nudo <snudo2288@stu.d214.org>
2020-10-20 15:19:07 -0500

    Initial Commit

---
+++ GitHubPrac1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPrac1
+//
+//  Created by Sam Nudo on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
